6d03ca955a5e2edb503d4a3838b1022df6bc93f8
Atiqur <atiqur@itconquest.com>
2026-02-21 15:12:33 +0600

    for make properties based projects and we can change the key values anytime

--- src/main/java/com/example/springtest/config/PusherConfig.java
+++ src/main/java/com/example/springtest/config/PusherConfig.java
@@ -1,16 +1,27 @@
 package com.example.springtest.config;
 
 import com.pusher.rest.Pusher;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "pusher")
+@Getter
+@Setter
 public class PusherConfig {
+
+    private String appId;
+    private String key;
+    private String secret;
+    private String cluster;
 
     @Bean
     public Pusher pusher() {
-        Pusher pusher = new Pusher("2106469", "e74dc87a03e4dbf5d60b", "defc8300bb55ed32d94f");
-        pusher.setCluster("ap1");
+        Pusher pusher = new Pusher(appId, key, secret);
+        pusher.setCluster(cluster);
         pusher.setEncrypted(true);
         return pusher;
     }
